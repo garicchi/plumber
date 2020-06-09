a04ba9a9628f561f6dd5d41f3f2db40480510299
togai-r <togai-r@klab.com>
2020-06-09 09:59:27 +0900

    fix:

---
+++ social-game/client/Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainGameManager : MonoBehaviour
+{
+    [SerializeField]
+    public Button BtnTitleStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BtnTitleStart.onClick.AddListener(() => {
+            OnTitleStartClick();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTitleStartClick() 
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+}
